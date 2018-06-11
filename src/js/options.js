c28b5552ee2ba4ddb092c66f14d7d5171739621f
YU-HSIN, CHEN <s63232@gmail.com>
2018-06-11 19:24:09 +0800

    update

--- src/js/options.js
+++ src/js/options.js
@@ -1,15 +1,19 @@
 import '../css/options.css';
 import App from './main/App';
-import Handlebars from './3rd/handlebars';
-import Unsplash, { toJson } from 'unsplash-js';
+import country_list from 'country-list';
+import lifeData from './life.json';
+// import Unsplash, { toJson } from 'unsplash-js';
 // import UnsplashHandler from './main/UnsplashHandler';
+console.log('---------');
+console.log(lifeData.filter(data => data.country === `Iceland`));
+console.log('---------');
+const countries = country_list();
 
-const unsplashInstance = new Unsplash({
-  applicationId:
-    'de67f1ba6f631670eedefff4c31bc09c840310d39a12d6785c1eb0f06fb7146f',
-  secret: '6e563cd7a8569dca86a605c7266e668d12c820a4568ca9eea254ff379df3004b',
-  callbackURL: 'urn:ietf:wg:oauth:2.0:oob',
-});
+// const unsplashInstance = new Unsplash({
+//   applicationId: 'de67f1ba6f631670eedefff4c31bc09c840310d39a12d6785c1eb0f06fb7146f',
+//   secret: '6e563cd7a8569dca86a605c7266e668d12c820a4568ca9eea254ff379df3004b',
+//   callbackURL: 'urn:ietf:wg:oauth:2.0:oob',
+// });
 
 let innerW = window.innerWidth;
 let innerH = window.innerHeight;
