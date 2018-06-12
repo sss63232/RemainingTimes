5e299dd4d7126a3e059046dd49baca2c350f551d
ChenYuHsin <s63232@gmail.com>
2018-06-12 08:42:55 +0800

    update

--- src/js/options.js
+++ src/js/options.js
@@ -2,12 +2,63 @@ import '../css/options.css';
 import App from './main/App';
 import country_list from 'country-list';
 import lifeData from './life.json';
+
 // import Unsplash, { toJson } from 'unsplash-js';
 // import UnsplashHandler from './main/UnsplashHandler';
-console.log('---------');
-console.log(lifeData.filter(data => data.country === `Iceland`));
-console.log('---------');
+
+// console.log(lifeData.filter(data => data.country === `Iceland`));
+
 const countries = country_list();
+
+let location;
+navigator.geolocation.getCurrentPosition(data => {
+  const { latitude, longitude } = data.coords;
+  const latlng = `${latitude},${longitude}`;
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng}`)
+    .then(res => res.json())
+    .then(json => {
+      const a = getCountry(json.results);
+      console.log('--------');
+      console.log(a);
+      console.log('--------');
+    });
+});
+
+//https://developers.google.com/maps/documentation/geocoding/intro?csw=1
+function getCountry(results) {
+  for (var i = 0; i < results[0].address_components.length; i++) {
+    var shortname = results[0].address_components[i].short_name;
+    var longname = results[0].address_components[i].long_name;
+    var type = results[0].address_components[i].types;
+    if (type.indexOf('country') != -1) {
+      if (!isNullOrWhitespace(shortname)) {
+        return shortname;
+      } else {
+        return longname;
+      }
+    }
+  }
+}
+
+function isNullOrWhitespace(text) {
+  if (text == null) {
+    return true;
+  }
+  return text.replace(/\s/gi, '').length < 1;
+}
+
+// fetch(`http://ip-api.com/json`)
+//   .then(data => data.json())
+//   .then(json => {
+//     console.log('--------');
+//     console.log(json);
+//     console.log('--------');
+//   })
+//   .catch(error => {
+//     console.log('--------');
+//     console.log(error);
+//     console.log('--------');
+//   });
 
 // const unsplashInstance = new Unsplash({
 //   applicationId: 'de67f1ba6f631670eedefff4c31bc09c840310d39a12d6785c1eb0f06fb7146f',
